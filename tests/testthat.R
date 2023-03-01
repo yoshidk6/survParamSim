89d5f0de8c36047359c364b66d616894d72bd8cf
Kenta Yoshida <yoshida.kenta.6@gmail.com>
2023-03-01 08:40:29 -0800

    Update testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,12 @@
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where should you do additional test configuration?
+# Learn more about the roles of various files in:
+# * https://r-pkgs.org/tests.html
+# * https://testthat.r-lib.org/reference/test_package.html#special-files
+
 library(testthat)
 library(survParamSim)
 
-# options(lifecycle_verbosity = "error")
 test_check("survParamSim")
